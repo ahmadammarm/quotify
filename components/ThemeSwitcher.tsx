fcecba1a47c5597ae66fe27609556e2f12589e10
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-09 10:35:11 +0700

    update light and dark mode toggle

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from "next-themes";
 import React, { useEffect, useState } from "react";
 import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
-import { MoonIcon, SunIcon } from "lucide-react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
