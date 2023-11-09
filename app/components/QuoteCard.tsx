91d9ac04c8aae1ecaa7a26c0c127211e7d14d8c1
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-11-09 08:15:28 +0700

    update quote card

--- app/components/QuoteCard.tsx
+++ app/components/QuoteCard.tsx
@@ -9,16 +9,22 @@ const QuoteCard = () => {
   const [quote, setQuote] = useState<string>(
     "Even what looks bad today, could be the best for tomorrow. We just have to be patient until His provisions arrive."
   );
-  const [author, setAuthor] = useState<string>("Ahmad Ammar");
+  const [author, setAuthor] = useState<string>("Someone");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const randomQuote = async () => {
+    setIsLoading(true);
     try {
       const response = await fetch("https://api.quotable.io/random");
       const data = await response.json();
-      setQuote(data.content);
-      setAuthor(data.author);
+      setTimeout(() => {
+        setQuote(data.content);
+        setAuthor(data.author);
+        setIsLoading(false);
+      }, 2000);
     } catch (error) {
       console.error("Error fetching random quote:", error);
+      setIsLoading(false);
     }
   };
 
@@ -34,7 +40,6 @@ const QuoteCard = () => {
     toast.success("Copied to your clipboard!");
   };
 
-
   return (
     <div className="w-full max-w-2xl mx-auto p-8">
       <header className="text-center text-3xl font-bold mb-8">
@@ -43,7 +48,7 @@ const QuoteCard = () => {
       <div className="relative flex flex-col justify-between items-center h-64">
         <div className="flex flex-col justify-center items-center text-center text-lg mb-4">
           <QuoteIcon className="text-2xl mb-4" />
-            <p className="quote text-center text-2xl">{quote}</p>
+          <p className="quote text-center text-2xl">{quote}</p>
           <QuoteIcon className="text-2xl mt-4 transform rotate-180" />
         </div>
         <div className="flex flex-col justify-center items-center text-center text-lg">
@@ -56,27 +61,19 @@ const QuoteCard = () => {
                 className="border rounded-full p-2 cursor-pointer transition duration-300 ease-in-out mr-2 hover:text-teal-500"
                 onClick={playSound}
               >
-                <Volume2 className="
-                hover:text-teal-500
-                transition
-                duration-300
-                ease-in-out
-                " />
+                <Volume2 className="hover:text-teal-500 transition duration-300 ease-in-out" />
               </li>
               <li
                 className="border rounded-full p-2 cursor-pointer transition duration-300 ease-in-out hover:text-teal-500"
                 onClick={copyQuote}
               >
-                <CopyIcon className="
-                hover:text-teal-500
-                transition
-                duration-300
-                ease-in-out
-                " />
+                <CopyIcon className="hover:text-teal-500 transition duration-300 ease-in-out" />
                 <Toaster />
               </li>
             </ul>
-            <Button onClick={randomQuote}>New Quote</Button>
+            <Button onClick={randomQuote} disabled={isLoading}>
+              {isLoading ? "Loading..." : "New Quote"}
+            </Button>
           </div>
         </div>
       </div>
